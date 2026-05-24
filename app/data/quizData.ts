46865edfff94020643c47406df477b50078a86bc
sabrinaismail12 <hibo2450@hotmail.com>
2026-05-25 00:50:35 +0200

    Add quiz data structure

---
+++ app/data/quizData.ts
@@ -0,0 +1,154 @@
+export const quizData = {
+  "fasalada-1-4": {
+    title: "Fasallada 1-4",
+    topics: [
+      {
+        topic: "Tirinta iyo tirooyinka",
+        subtopics: [
+          {
+            title: "Tirinta 1 ilaa 100",
+            questions: [
+              {
+                question: "Immisa ayaa ah 7 + 5?",
+                options: ["10", "11", "12", "13"],
+                answer: "12",
+              },
+              {
+                question: "Tirada ka dambeysa 39 waa maxay?",
+                options: ["38", "40", "41", "49"],
+                answer: "40",
+              },
+            ],
+          },
+          {
+            title: "Isbarbardhigga tirooyinka",
+            questions: [
+              {
+                question: "Kee baa weyn: 24 mise 42?",
+                options: ["24", "42", "Labaduba waa isku mid", "Midna"],
+                answer: "42",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        topic: "Isku dar iyo kala jar",
+        subtopics: [
+          {
+            title: "Isku darka fudud",
+            questions: [
+              {
+                question: "15 + 8 waa imisa?",
+                options: ["21", "22", "23", "24"],
+                answer: "23",
+              },
+            ],
+          },
+          {
+            title: "Kala jarka fudud",
+            questions: [
+              {
+                question: "20 - 6 waa imisa?",
+                options: ["12", "13", "14", "15"],
+                answer: "14",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+
+  "fasalada-5-8": {
+    title: "Fasallada 5-8",
+    topics: [
+      {
+        topic: "Jajabyo",
+        subtopics: [
+          {
+            title: "Fahamka jajabyada",
+            questions: [
+              {
+                question: "1/2 + 1/2 waa imisa?",
+                options: ["1/4", "1", "2", "1/2"],
+                answer: "1",
+              },
+              {
+                question: "Jajabka 3/4 muxuu ka dhigan yahay?",
+                options: [
+                  "3 qaybood oo ka mid ah 4 qaybood",
+                  "4 qaybood oo ka mid ah 3 qaybood",
+                  "3 + 4",
+                  "4 - 3",
+                ],
+                answer: "3 qaybood oo ka mid ah 4 qaybood",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        topic: "Aljabra bilow ah",
+        subtopics: [
+          {
+            title: "Xallinta x",
+            questions: [
+              {
+                question: "Haddii x + 5 = 12, x waa imisa?",
+                options: ["5", "6", "7", "8"],
+                answer: "7",
+              },
+              {
+                question: "Haddii 3x = 18, x waa imisa?",
+                options: ["3", "5", "6", "9"],
+                answer: "6",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+
+  "fasalada-9-12": {
+    title: "Fasallada 9-12",
+    topics: [
+      {
+        topic: "Aljabra",
+        subtopics: [
+          {
+            title: "Ligninger",
+            questions: [
+              {
+                question: "Xalli: 2x + 4 = 14",
+                options: ["4", "5", "6", "7"],
+                answer: "5",
+              },
+              {
+                question: "Xalli: x² = 49",
+                options: ["5", "6", "7", "8"],
+                answer: "7",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        topic: "Geometri",
+        subtopics: [
+          {
+            title: "Areal",
+            questions: [
+              {
+                question: "Arealka afargeesle leh dherer 8 iyo ballac 5 waa imisa?",
+                options: ["13", "30", "40", "45"],
+                answer: "40",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
